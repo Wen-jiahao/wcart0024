6d082635015af1e73f0c4b93ce98808434359efa
wen <100@qq.com>
2020-02-25 20:34:40 +0800

    image

--- wcart-administration-back/src/main/java/io/wjh/wcartadministrationback/controller/ImageController.java
+++ wcart-administration-back/src/main/java/io/wjh/wcartadministrationback/controller/ImageController.java
@@ -1,10 +1,16 @@
 package io.wjh.wcartadministrationback.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/Image")
 public class ImageController {
-
+    @PostMapping("/upload")
+    public String upload(@RequestParam MultipartFile image){
+        return null;
+    }
 }

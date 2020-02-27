8bf2621d9896b9ce9fae4edae10bd1152a8adb4e
wen <100@qq.com>
2020-02-27 20:52:15 +0800

    create front

---
+++ jcart-administration-front/viewmodels/product-create.js
@@ -0,0 +1,114 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        productCode : '',
+        productName : '',
+        price : '',
+        discount : '',
+        stockQuantity : '',
+        rewordPoints : '',
+        sortOrder : '',
+        description : '',
+        selectedStatus:1,
+        selectedMainPic:'',
+        selectedOtherPics: [],
+        otherPicUrls: [],
+        mainPicUrl : '',
+        othertPicUrls :[],
+        status :'',
+        statuses : [{ 
+            value: '0',
+            label: '上架'
+          }, {
+            value: '1',
+            label: '下架'
+          }, {
+            value: '2',
+            label: '待审核'
+          }],
+          mainfileList:[],
+          otherfileList :[]
+    },
+    methods:{
+        handleCreateClick(){
+            console.log('create click');
+            this.createProduct();
+        },
+        handleOnMainChange(val){
+            this.selectedMainPic=val.raw;
+        },
+        handleUploadMainClick(){
+            this.uploadMainImage();
+        },
+        uploadMainImage() {
+            var formData = new FormData();
+            formData.append("image", this.selectedMainPic);
+
+            axios.post('/Image/upload', formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            })
+                .then(function (response) {
+                    console.log(response);
+                    app.mainPicUrl = response.data;
+                    alert('上传成功');
+                })
+                .catch(function (error) {
+                    console.log(error);
+                    alert('上传失败');
+                });
+        },
+        handleOnOtherChange(file,fileList){
+            this.selectedOtherPics=fileList;
+        },
+        handleUploadOtherClick(){
+                this.uploadOtherImage();
+        },
+        uploadOtherImage() {
+            this.selectedOtherPics.forEach(pic => {
+                var formData = new FormData();
+                formData.append("image", pic.raw);
+
+                axios.post('/Image/upload', formData, {
+                    headers: {
+                        'Content-Type': 'multipart/form-data'
+                    }
+                })
+                    .then(function (response) {
+                        console.log(response);
+                        var url = response.data;
+                        app.otherPicUrls.push(url);
+                    })
+                    .catch(function (error) {
+                        console.log(error);
+                        alert('上床失败');
+                    });
+            });
+
+
+        },
+        createProduct() {
+            axios.post('/product/create', {
+                productCode : this.productCode,
+                productName : this.productName,
+                price : this.price,
+                discount : this.discount,
+                stockQuantity : this.stockQuantity,
+                rewordPoints : this.rewordPoints,
+                sortOrder : this.sortOrder,
+                description : this.description,
+                status :this.status,
+                mainPicUrl : this.mainPicUrl,
+                othertPicUrls :this.otherPicUrls
+              })
+              .then(function (response) {
+                console.log('创建成功');
+              })
+              .catch(function (error) {
+                console.log(error);
+              });
+        }
+        
+    }
+})
--- wcart-administration-back/src/main/java/io/wjh/wcartadministrationback/controller/ImageController.java
+++ wcart-administration-back/src/main/java/io/wjh/wcartadministrationback/controller/ImageController.java
@@ -2,10 +2,7 @@ package io.wjh.wcartadministrationback.controller;
 
 import io.wjh.wcartadministrationback.constant.ClientExceptionConstant;
 import io.wjh.wcartadministrationback.exception.ClientException;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.FileNotFoundException;
@@ -17,6 +14,7 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/Image")
+@CrossOrigin
 public class ImageController {
 
     private List<String> imageExts= Arrays.asList("jpg","jpeg","png");

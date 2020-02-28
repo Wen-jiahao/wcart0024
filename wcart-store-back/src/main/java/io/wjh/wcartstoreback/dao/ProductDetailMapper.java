3810228e3c04b958db373c527d494bfa3a99602c
wen <100@qq.com>
2020-02-28 21:08:40 +0800

    add store getbyid back

--- wcart-store-back/src/main/java/io/wjh/wcartstoreback/dao/ProductDetailMapper.java
+++ wcart-store-back/src/main/java/io/wjh/wcartstoreback/dao/ProductDetailMapper.java
@@ -1,7 +1,9 @@
 package io.wjh.wcartstoreback.dao;
 
 import io.wjh.wcartstoreback.po.ProductDetail;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductDetailMapper {
     int deleteByPrimaryKey(Integer productId);
 
--- wcart-store-back/src/main/java/io/wjh/wcartstoreback/service/ProductService.java
+++ wcart-store-back/src/main/java/io/wjh/wcartstoreback/service/ProductService.java
@@ -2,7 +2,10 @@ package io.wjh.wcartstoreback.service;
 
 import com.github.pagehelper.Page;
 import io.wjh.wcartstoreback.dto.out.ProductListOutDTO;
+import io.wjh.wcartstoreback.dto.out.ProductShowOutDTO;
 
 public interface ProductService {
     Page<ProductListOutDTO> search(Integer pageNum);
+
+    ProductShowOutDTO getById(Integer productId);
 }

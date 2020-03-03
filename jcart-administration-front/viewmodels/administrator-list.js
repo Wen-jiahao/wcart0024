9448757a7409e610f7bf944717543cc7a4f14c24
wen <100@qq.com>
2020-03-03 16:00:18 +0800

    batchDelete administrator page

--- jcart-administration-front/viewmodels/administrator-list.js
+++ jcart-administration-front/viewmodels/administrator-list.js
@@ -2,7 +2,13 @@ var app = new Vue({
     el: '#app',
     data: {
         pageInfo:'',
-        pageNum:1
+        pageNum:1,
+        selectedAdministrators: []
+    },
+    computed:{
+        selectedAdministratorIds(){
+            return this.selectedAdministrators.map(a=> a.administratorId);
+        }
     },
     mounted(){
         console.log('view mouned');
@@ -46,7 +52,26 @@ var app = new Vue({
               .catch(function (error) {
                 console.log(error);
               });
+        },
+        handleSelectionChange(val){
+            console.log('change select');
+            this.selectedAdministrators = val;
+        },
+        handleBatchDeleteClick(){
+            this.batchDeleteAdministrators();
+        },
+        batchDeleteAdministrators() {
+            axios.post('/administrator/batchDelete', this.selectedAdministratorIds)
+                .then(function (response) {
+                    console.log(response);
+                    alert('批删成功');
+                    location.reload();
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
         }
+
 
     }
 

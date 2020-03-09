0ecc0474868a348bfdb5e60be03d4f712b2109d2
wen <100@qq.com>
2020-03-09 17:51:57 +0800

    getPwdResetCode page

---
+++ jcart-administration-front/viewmodels/administrator-getPwdResetCode.js
@@ -0,0 +1,30 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        email: ''
+    },
+    methods:{
+        handleFindBackPwdClick(){
+            console.log('find back pwd click');
+            this.getPwdRecord();
+        },
+        getPwdRecord(){
+            axios.get('/administrator/getPwdResetCode', {
+                params: {
+                    email: this.email
+                }
+            })
+                .then(function (response) {
+                    console.log(response);
+                    alert('重置码已发送到邮箱');
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        }
+        }
+
+        
+
+    
+}) 

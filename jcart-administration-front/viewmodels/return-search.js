085ed44a39150da94786332f09dd5282b5b8ad46
wen <100@qq.com>
2020-03-10 19:09:14 +0800

    return-search page

--- jcart-administration-front/viewmodels/return-search.js
+++ jcart-administration-front/viewmodels/return-search.js
@@ -2,22 +2,58 @@ var app = new Vue({
     el: '#app',
     data: {
         pageInfo: '',
-        pageNum: 1
+        pageNum: 1,
+        returnId: '',
+        orderId: '',
+        customerName: '',
+        productCode: '',
+        productName: '',
+        selectstatus: '',
+        startTime:'',
+        endTime:'',
+        statuses:[
+            { value: 0, label: '待取货' },
+            { value: 1, label: '正在处理' },
+            { value: 2, label: '完成' },
+            { value: 3, label: '拒绝' }
+        ]
     },
     mounted() {
         console.log('view mounted');
         this.searchReturn();
     },
     methods: {
+        handleSerachClick(){
+            this.pageNum=1;
+            this.searchReturn();
+        },
         handlePageChange(val) {
             console.log('page changed', val);
             this.pageNum = val;
             this.searchReturn();
         },
+        handleClearClick(){
+            this.returnId = '';
+            this.orderId = '';
+            this.customerName = '';
+            this.productCode = '';
+            this.productName = '';
+            this.selectedStatus = '';
+            this.startTime = '';
+            this.endTime = '';
+        },
         searchReturn() {
             axios.get('/return/search', {
                 params: {
-                    pageNum: this.pageNum
+                    pageNum: this.pageNum,
+                    returnId: this.returnId,
+                    orderId: this.orderId,
+                    customerName: this.customerName,
+                    productCode: this.productCode,
+                    productName: this.productName,
+                    status: this.selectstatus,
+                    startTimestamp:this.startTime?this.startTime.getTime():'',
+                    endTimestamp:this.endTime?this.endTime.getTime():'',
                 }
             })
                 .then(function (response) {

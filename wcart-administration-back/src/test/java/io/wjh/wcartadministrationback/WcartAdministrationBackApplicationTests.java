dd2feb0e0ba50cd69a3dc941549c393b641451ae
wen <100@qq.com>
2020-03-16 14:19:01 +0800

    junit test

--- wcart-administration-back/src/test/java/io/wjh/wcartadministrationback/WcartAdministrationBackApplicationTests.java
+++ wcart-administration-back/src/test/java/io/wjh/wcartadministrationback/WcartAdministrationBackApplicationTests.java
@@ -1,13 +1,25 @@
 package io.wjh.wcartadministrationback;
 
+import io.wjh.wcartadministrationback.po.Administrator;
+import io.wjh.wcartadministrationback.service.AdministratorService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class WcartAdministrationBackApplicationTests {
 
+    @Autowired
+    private AdministratorService administratorService;
     @Test
     void contextLoads() {
+        String username="wjh";
+        Administrator administratorServiceByUsername = administratorService.getByUsername(username);
+        assertNotNull(administratorServiceByUsername);
+        String username1="wjh1";
+        Administrator administratorServiceByUsername1 = administratorService.getByUsername(username1);
+        assertNotNull(administratorServiceByUsername1);
     }
 
 }
---
+++ wcart-administration-back/src/test/java/io/wjh/wcartadministrationback/controller/AdministratorControllerTest.java
@@ -0,0 +1,12 @@
+package io.wjh.wcartadministrationback.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AdministratorControllerTest {
+
+    @Test
+    void login() {
+    }
+}

6740d7f45fd03a37efa8f5e0699f4ef704503264
wen <100@qq.com>
2020-03-20 16:59:45 +0800

    first suppot

---
+++ wcart-administration-back/src/main/java/io/wjh/wcartadministrationback/mq/EmailEvent.java
@@ -0,0 +1,32 @@
+package io.wjh.wcartadministrationback.mq;
+
+public class EmailEvent {
+
+    private String fromEmail;
+    private String email;
+    private String hex;
+
+    public String getFromEmail() {
+        return fromEmail;
+    }
+
+    public void setFromEmail(String fromEmail) {
+        this.fromEmail = fromEmail;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getHex() {
+        return hex;
+    }
+
+    public void setHex(String hex) {
+        this.hex = hex;
+    }
+}
---
+++ wcart-suppot-back/src/main/java/io/wjh/wcartsuppotback/WcartSuppotBackApplication.java
@@ -0,0 +1,13 @@
+package io.wjh.wcartsuppotback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WcartSuppotBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WcartSuppotBackApplication.class, args);
+    }
+
+}
